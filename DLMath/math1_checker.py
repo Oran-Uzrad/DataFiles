d4ad5a6f429248370d26c4bc6051a32970d78f39
Oran-Uzrad <50901272+Oran-Uzrad@users.noreply.github.com>
2021-08-21 19:41:33 +0300

    dg_dd

--- DLMath/math1_checker.py
+++ DLMath/math1_checker.py
@@ -44,7 +44,7 @@ class math1_checker:
       print('\033[91m Test 1: Failed')
 
   def test_ex7(self , f1 , f2):
-    if f1(9) - f2(-4) == self.dg(9) - self.dg(-4):
+    if f1(9) - f2(-4) == self.dg(9) - self.dg_dd(-4):
       print('\033[92m Test 1: Passed')
     else:
       print('\033[91m Test 1: Failed')
