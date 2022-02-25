aaf2506beb3ea82a6474b7acaa449fe658447551
Oran-Uzrad <50901272+Oran-Uzrad@users.noreply.github.com>
2022-02-25 12:06:14 +0200

    005

--- DLMath/u12.py
+++ DLMath/u12.py
@@ -11,7 +11,7 @@ def plot_decision_boundary(model, X, y , sklearn = False):
     # Set min and max values and give it some padding
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
     y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
-    h = 0.1
+    h = 0.05
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     # Predict the function value for the whole grid
